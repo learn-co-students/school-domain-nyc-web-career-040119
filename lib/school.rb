06944247c12c842bb98f252210da7f9835f069e9
ferrisboran <ferris@ferryrules.com>
2019-04-02 17:43:21 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :model, :roster, :add_student, :grade
+
+  def initialize(model)
+    @model = model
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+    end
+    if !@roster[grade].include?(name)
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    #@roster.sort.to_h
+    @roster.each do |grade, names|
+      @roster[grade] = @roster[grade].sort
+    end
+  end
+
+end
