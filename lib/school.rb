6faff7826389e07f8a35f4899d47a566052cf9f8
Gamerman2001 <Tro12185@gmail.com>
2019-04-03 16:56:09 -0400

    lab done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+class School
+    attr_accessor :roster
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(student,grade)
+        if @roster.has_key?(grade) 
+            @roster[grade] << student  
+        elsif       
+            @roster[grade] = []
+            @roster[grade] << student
+        end 
+    end 
+
+    def grade(grade) 
+        @roster[grade]
+    end 
+
+    def sort
+        @roster.values.collect do |names|
+        names.sort!
+    end
+        @roster
+    end 
+end
